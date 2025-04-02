7c73632ba3798e2ee56921eeba6a84a3f109b7ce
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-03 00:04:42 +0530

    Add files via upload

---
+++ Assignment7.py
@@ -0,0 +1,30 @@
+"""
+# Q1
+x=int(input("Enter a number "))
+y=bin(x)
+print(y)
+
+#Q2
+x=0b1100101
+y=int(x)
+print(y)
+
+
+#Q3
+x=0x2F
+y=oct(x)
+print(y)
+
+
+#Q4
+x=0o125
+y=bin(x)
+print(y)
+
+"""
+#Q5
+x=0o25
+y=0x39
+sum=x+y
+print(bin(sum))
+
